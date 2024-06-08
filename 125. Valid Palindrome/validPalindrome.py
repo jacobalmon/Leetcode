47b4669a6775a54663f2f0a1e94adf12b7ddde39
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-06-07 18:19:02 -0700

    Update validPalindrome.py

--- 125. Valid Palindrome/validPalindrome.py
+++ 125. Valid Palindrome/validPalindrome.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def isPalindrome(self, s):
-        left = 0
-        right = len(s) - 1
+        left = 0 # Left Pointer at the first index.
+        right = len(s) - 1 # Right Pointer at the last index.
         
+        # Loop until the pointer meet or past each other.
         while left < right:
 
+            # Move Left Pointer until its a alphanumeric character.
             while left < right and not s[left].isalnum():
                 left += 1
-
+            
+            # Move Right Pointer until its a alphanumeric character.
             while left < right and not s[right].isalnum():
                 right -= 1
 
+            # Character are not the same, its not palindrome.
             if s[left].lower() != s[right].lower():
                 return False
 
-            left += 1
-            right -= 1
+            left += 1 # Increment Left Pointer.
+            right -= 1 # Increment Right Pointer.
         
+        # If the string has been ran through completely, then its a palindrome.
         return True
