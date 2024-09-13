8a6422f3800538c673100ee1ae8f2364b13a1920
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-09-12 21:42:59 -0700

    Update stock1.py

--- 121. Best Time to Buy and Sell Stock/stock1.py
+++ 121. Best Time to Buy and Sell Stock/stock1.py
@@ -1,15 +1,19 @@
 class Solution(object):
     def maxProfit(self, prices):
+        # Initialize Max Profit to 0, in the case that there is no max profit.
         max_profit = 0
-        left = 0
-        right = 1
+        left = 0 # representing the index we buy stock.
+        right = 1 # representing the index we sell stock.
 
+        # Iteratring over the list until we reached our max profit.
         while right < len(prices):
+            # Ensures the current profit is postive.
             if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
-                if profit > max_profit: max_profit = profit
+                # Get our max profit from that right's iteration.
+                max_profit = max(max_profit, profit)
             else:
-                left = right
-            right += 1
+                left = right # We want to buy at the lowest price.
+            right += 1 # We check all the places we want to sell.
 
         return max_profit
